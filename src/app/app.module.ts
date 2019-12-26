880ae3e6c0944f7541c005c655f9492a0f0ed8cb
Lizeth Patiño <lizeth0330@outlook.com>
2019-12-26 13:44:02 -0500

    Creación del servicio de listar personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarComponent } from './Persona/listar/listar.component';
@@ -16,7 +16,8 @@ import { EditComponent } from './Persona/edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
